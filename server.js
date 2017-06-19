650f3bb93d0500215398ef29d1343f2c66a7e6ba
Operation Spark <operationspark@Operations-MacBook-Pro-4.local>
2017-06-19 14:10:47 -0500

    added index file, server that makes a get request that reads from index file

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('Revieeeewwwww!')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+console.log('Revieeeewwwww!');
