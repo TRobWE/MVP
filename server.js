c844574eefdcc0898421c8afad5f931ced4d6298
Operation Spark <operationspark@Operations-MacBook-Pro-4.local>
2017-06-19 20:30:27 -0500

    update index install parser mongoose, update server file

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 const express = require('express');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 
-app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
+const bodyParser = require('body-parser');
+
+const MongoClient = require('mongodb').MongoClient;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let db;
+
+MongoClient.connect('mongodb://trobwe:grapp@ds131782.mlab.com:31782/grapp', (err, database) => {
+  if (err) {
+    return err;
+  }
+  db = database;
+  app.listen(PORT, () => {});
+  return db;
+});
+
+app.post('/watchList', (req, res) => {
+  db.collection('watchList').save(req.body, (err, result) => {
+    if (err) {
+      return err;
+    }
+    res.redirect('/');
+    return result;
+  });
 });
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-console.log('Revieeeewwwww!');
